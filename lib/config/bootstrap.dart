2b274f5fc81f58e08d4eb803502a38a39da326e0
Abuzar Rasool <abuzar@oraan.com>
2023-01-12 18:30:38 +0500

    HAZ-17 Firebase setup complete

--- lib/config/bootstrap.dart
+++ lib/config/bootstrap.dart
@@ -1,12 +1,18 @@
 import 'dart:async';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hazir_app/config/logger.dart';
+import 'package:hazir_app/firebase_options.dart';
 
 class Bootstrap {
   static Future<void> wrap(FutureOr<Widget> Function() builder) async {
+    
     await runZonedGuarded(
       () async {   
         WidgetsFlutterBinding.ensureInitialized();
+        await Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        );
         runApp(await builder());
 
       },
---
+++ lib/presentation/screens/login_screen/local_widgets/login_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  static String get screenName => "login";
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
